8abf759d55cdf942da2dd24288d96bee814d6735
dbehdrbs0806 <a67422830@gmail.com>
2024-01-04 02:26:28 +0900

    test

---
+++ spring/src/main/java/com/example/spring/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Helllo World";
+    }
+}
